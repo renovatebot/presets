dc7617223a9269f0247a1c0ea0878e0a9979d20f
Rhys Arkins <rhys@arkins.net>
2018-02-02 11:34:22 +0100

    fix: use more specific all grouping

--- packages/renovate-config-group/static-groups.js
+++ packages/renovate-config-group/static-groups.js
@@ -1,7 +1,12 @@
 module.exports = {
   all: {
     description: 'Group all updates together',
-    groupName: 'all',
+    packageRules: [
+      {
+        packagePatterns: ['*'],
+        groupName: 'all',
+      },
+    ],
     lockFileMaintenance: {
       enabled: false,
     },
