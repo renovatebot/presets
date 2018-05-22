1445c8b8dc5c9c2efe6ffeb7c036de91120f1221
Rhys Arkins <rhys@arkins.net>
2018-05-22 09:13:00 +0200

    Update generate.js

--- packages/renovate-config-monorepo/generate.js
+++ packages/renovate-config-monorepo/generate.js
@@ -80,7 +80,7 @@ const dynamicSources = {
     repo: 'babel/babel',
   },
   graphqlcodegenerator: {
-    repo: 'dotansimha/graphql-code-generator'
+    repo: 'dotansimha/graphql-code-generator',
   },
   jest: {
     repo: 'facebook/jest',
