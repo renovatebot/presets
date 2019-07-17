ab6976ca3860d9cdb156f225f9951cb1540f01a3
Rhys Arkins <rhys@arkins.net>
2019-07-17 16:05:25 +0200

    refactor monorepos (#130)

--- packages/renovate-config-monorepo/generate.js
+++ packages/renovate-config-monorepo/generate.js
@@ -1,358 +1,108 @@
+const is = require('@sindresorhus/is');
 const fs = require('fs');
-const got = require('gh-got');
 const path = require('path');
 const pJson = require('./package.json');
 
-const staticSources = {
-  angular1: {
-    packageNames: [
-      'angular',
-      'angular-animate',
-      'angular-aria',
-      'angular-cookies',
-      'angular-loader',
-      'angular-message-format',
-      'angular-messages',
-      'angular-mocks',
-      'angular-parse-ext',
-      'angular-resource',
-      'angular-route',
-      'angular-sanitize',
-      'angular-scenario',
-      'angular-touch',
-      '@types/angular',
-      '@types/angular-animate',
-      '@types/angular-aria',
-      '@types/angular-cookies',
-      '@types/angular-loader',
-      '@types/angular-message-format',
-      '@types/angular-messages',
-      '@types/angular-mocks',
-      '@types/angular-parse-ext',
-      '@types/angular-resource',
-      '@types/angular-route',
-      '@types/angular-sanitize',
-      '@types/angular-scenario',
-      '@types/angular-touch',
-    ],
-  },
-  commitlint: {
-    packageNames: ['commitlint'],
-    packagePatterns: ['^@commitlint/'],
-  },
-  'semantic-release': {
-    packageNames: ['semantic-release'],
-    packagePatterns: ['^@semantic-release/'],
-  },
-  lodash: {
-    packageNames: ['babel-plugin-lodash', 'lodash-webpack-plugin', 'lodash-es'],
-    packagePatterns: ['^lodash'],
-  },
-  neutrino: {
-    packageNames: ['neutrino'],
-    packagePatterns: ['^@neutrinojs/'],
-  },
-  ngrx: {
-    packageNames: ['ngrx'],
-    packagePatterns: ['^@ngrx/'],
-  },
-  nrwl: {
-    packageNames: ['nrwl'],
-    packagePatterns: ['^@nrwl/'],
-  },
-  storybook: {
-    packagePatterns: ['^@storybook/'],
-  },
-  fimbullinter: {
-    packagePatterns: ['^@fimbul/'],
-  },
-  wordpress: {
-    packagePatterns: ['^@wordpress/'],
-  },
-  angularmaterial: {
-    packageNames: ['@angular/material', '@angular/cdk'],
-  },
-  angularcli: {
-    sourceUrlPrefixes: ['https://github.com/angular/angular-cli'],
-  },
-  'bugsnag-js': {
-    sourceUrlPrefixes: ['https://github.com/bugsnag/bugsnag-js'],
-  },
-  flopflip: {
-    sourceUrlPrefixes: ['https://github.com/tdeekens/flopflip'],
-  },
-  uppy: {
-    sourceUrlPrefixes: ['https://github.com/transloadit/uppy'],
-  },
-  strapi: {
-    sourceUrlPrefixes: ['https://github.com/strapi/strapi'],
-  },
-  'telus-tds': {
-    sourceUrlPrefixes: ['https://github.com/telusdigital/tds'],
-  },
-  'devextreme-reactive': {
-    sourceUrlPrefixes: ['https://github.com/DevExpress/devextreme-reactive'],
-  },
-  'react-dnd': {
-    sourceUrlPrefixes: ['https://github.com/react-dnd/react-dnd'],
-  },
-  'ag-grid': {
-    sourceUrlPrefixes: ['https://github.com/ag-grid/ag-grid'],
-  },
-  stryker: {
-    sourceUrlPrefixes: ['https://github.com/stryker-mutator/stryker'],
-  },
-  'typography-js': {
-    sourceUrlPrefixes: ['https://github.com/KyleAMathews/typography.js'],
-  },
-  'aws-cdk': {
-    sourceUrlPrefixes: ['https://github.com/aws/aws-cdk'],
-  },
-  'aws-java-sdk': {
-    packagePatterns: ['^com.amazonaws:aws-java-sdk-'],
-  },
-  capacitor: {
-    sourceUrlPrefixes: ['https://github.com/ionic-team/capacitor'],
-  },
-  'algolia-react-instantsearch': {
-    sourceUrlPrefixes: ['https://github.com/algolia/react-instantsearch'],
-  },
-  typefaces: {
-    sourceUrlPrefixes: ['https://github.com/KyleAMathews/typefaces'],
-  },
-  expo: {
-    sourceUrlPrefixes: ['https://github.com/expo/expo'],
-  },
-  'ionic-native': {
-    sourceUrlPrefixes: ['https://github.com/ionic-team/ionic-native'],
-  },
-  'ngxs-store': {
-    sourceUrlPrefixes: ['https://github.com/ngxs/store'],
-  },
-  mdx: {
-    sourceUrlPrefixes: ['https://github.com/mdx-js/mdx'],
-  },
-  vuepress: {
-    sourceUrlPrefixes: ['https://github.com/vuejs/vuepress'],
-  },
-  workbox: {
-    sourceUrlPrefixes: ['https://github.com/googlechrome/workbox'],
-  },
-  sentry: {
-    sourceUrlPrefixes: ['https://github.com/getsentry/sentry-javascript'],
-  },
-  lingui: {
-    sourceUrlPrefixes: ['https://github.com/lingui/js-lingui'],
-  },
-  'typescript-eslint': {
-    sourceUrlPrefixes: [
-      'https://github.com/typescript-eslint/typescript-eslint',
-    ],
-  },
-  nest: {
-    sourceUrlPrefixes: ['https://github.com/nestjs/nest'],
-  },
-  'ember-decorators': {
-    sourceUrlPrefixes: ['https://github.com/ember-decorators/ember-decorators'],
-  },
-  'apollo-server': {
-    sourceUrlPrefixes: ['https://github.com/apollographql/apollo-server'],
-  },
-  surveyjs: {
-    sourceUrlPrefixes: ['https://github.com/surveyjs/surveyjs'],
-  },
-  'aspnet Extensions': {
-    sourceUrlPrefixes: ['https://github.com/aspnet/Extensions'],
-  },
-  'aspnet AspNetWebStack': {
-    sourceUrlPrefixes: ['https://github.com/aspnet/AspNetWebStack'],
-  },
-  'azure azure-libraries-for-net': {
-    sourceUrlPrefixes: ['https://github.com/Azure/azure-libraries-for-net'],
-  },
-  'azure azure-storage-net': {
-    sourceUrlPrefixes: ['https://github.com/Azure/azure-storage-net'],
-  },
-  'azure azure-sdk-for-net': {
-    sourceUrlPrefixes: ['https://github.com/Azure/azure-sdk-for-net'],
-  },
-  framework7: {
-    sourceUrlPrefixes: ['https://github.com/framework7io/framework7'],
-  },
-  emotion: {
-    sourceUrlPrefixes: ['https://github.com/emotion-js/emotion'],
-  },
-  nextjs: {
-    sourceUrlPrefixes: ['https://github.com/zeit/next.js'],
-  },
-  picasso: {
-    sourceUrlPrefixes: ['https://github.com/qlik-oss/picasso.js'],
-  },
-  clarity: {
-    sourceUrlPrefixes: ['https://github.com/vmware/clarity'],
-  },
-  infrastructure: {
-    sourceUrlPrefixes: ['https://github.com/instructure/instructure-ui'],
-  },
-  webdriverio: {
-    sourceUrlPrefixes: ['https://github.com/webdriverio/webdriverio'],
-  },
-  'reactivestack-cookies': {
-    sourceUrlPrefixes: ['https://github.com/reactivestack/cookies'],
-  },
-  router5: {
-    sourceUrlPrefixes: ['https://github.com/router5/router5'],
-  },
-  lerna: {
-    sourceUrlPrefixes: ['https://github.com/lerna/lerna'],
-  },
-  remark: {
-    sourceUrlPrefixes: ['https://github.com/remarkjs/remark'],
-  },
-  'electron-forge': {
-    sourceUrlPrefixes: ['https://github.com/electron-userland/electron-forge'],
-  },
-  graphqlcodegenerator: {
-    sourceUrlPrefixes: [
-      'https://github.com/dotansimha/graphql-code-generator',
-      'https://github.com/dotansimha/graphql-codegen',
-    ],
-  },
-  nuxtjs: {
-    sourceUrlPrefixes: ['https://github.com/nuxt/nuxt.js'],
-  },
+const repoGroups = {
+  'ag-grid': 'https://github.com/ag-grid/ag-grid',
+  'algolia-react-instantsearch':
+    'https://github.com/algolia/react-instantsearch',
+  'apollo-server': 'https://github.com/apollographql/apollo-server',
+  'aspnet AspNetWebStack': 'https://github.com/aspnet/AspNetWebStack',
+  'aspnet Extensions': 'https://github.com/aspnet/Extensions',
+  'aws-cdk': 'https://github.com/aws/aws-cdk',
+  'azure azure-libraries-for-net':
+    'https://github.com/Azure/azure-libraries-for-net',
+  'azure azure-sdk-for-net': 'https://github.com/Azure/azure-sdk-for-net',
+  'azure azure-storage-net': 'https://github.com/Azure/azure-storage-net',
+  'bugsnag-js': 'https://github.com/bugsnag/bugsnag-js',
+  'devextreme-reactive': 'https://github.com/DevExpress/devextreme-reactive',
+  'electron-forge': 'https://github.com/electron-userland/electron-forge',
+  'ember-decorators': 'https://github.com/ember-decorators/ember-decorators',
+  'graphql-modules': 'https://github.com/Urigo/graphql-modules',
+  'ionic-native': 'https://github.com/ionic-team/ionic-native',
+  'mdc-react': 'material-components/material-components-web-react',
+  'ngxs-store': 'https://github.com/ngxs/store',
+  'react-dnd': 'https://github.com/react-dnd/react-dnd',
+  'reactivestack-cookies': 'https://github.com/reactivestack/cookies',
+  'semantic-release': 'https://github.com/semantic-release/',
+  'telus-tds': 'https://github.com/telusdigital/tds',
+  'typescript-eslint': 'https://github.com/typescript-eslint/typescript-eslint',
+  'typography-js': 'https://github.com/KyleAMathews/typography.js',
+  'vue-cli': 'https://github.com/vuejs/vue-cli',
+  accounts: 'https://github.com/accounts-js/accounts',
+  angular1: 'https://github.com/angular/angular.js',
+  angular: 'https://github.com/angular/angular',
+  angularcli: 'https://github.com/angular/angular-cli',
+  apolloclient: 'https://github.com/apollographql/apollo-client',
+  apollolink: 'https://github.com/apollographql/apollo-link',
+  awsappsync: 'https://github.com/awslabs/aws-mobile-appsync-sdk-js',
+  babel: 'https://github.com/babel/babel',
+  baset: 'https://github.com/igmat/baset',
+  capacitor: 'https://github.com/ionic-team/capacitor',
+  clarity: 'https://github.com/vmware/clarity',
+  commitlint: 'https://github.com/conventional-changelog/commitlint',
+  emotion: 'https://github.com/emotion-js/emotion',
+  expo: 'https://github.com/expo/expo',
+  fimbullinter: 'https://github.com/fimbullinter/wotan',
+  flopflip: 'https://github.com/tdeekens/flopflip',
+  framework7: 'https://github.com/framework7io/framework7',
+  gatsby: 'https://github.com/gatsbyjs/gatsby',
+  graphqlcodegenerator: [
+    'https://github.com/dotansimha/graphql-code-generator',
+    'https://github.com/dotansimha/graphql-codegen',
+  ],
+  infrastructure: 'https://github.com/instructure/instructure-ui',
+  jest: 'https://github.com/facebook/jest',
+  lerna: 'https://github.com/lerna/lerna',
+  lingui: 'https://github.com/lingui/js-lingui',
+  lodash: 'https://github.com/lodash/',
+  material: 'https://github.com/material-components/material-components-web',
+  mdx: 'https://github.com/mdx-js/mdx',
+  mui: 'https://github.com/mui-org/material-ui',
+  nest: 'https://github.com/nestjs/nest',
+  neutrino: 'https://github.com/neutrinojs/neutrino',
+  nextjs: 'https://github.com/zeit/next.js',
+  ngrx: 'https://github.com/ngrx/',
+  nrwl: 'https://github.com/nrwl/',
+  nuxtjs: 'https://github.com/nuxt/nuxt.js',
+  picasso: 'https://github.com/qlik-oss/picasso.js',
+  pouchdb: 'https://github.com/pouchdb/pouchdb',
+  react: 'https://github.com/facebook/react',
+  reactrouter: 'https://github.com/ReactTraining/react-router',
+  remark: 'https://github.com/remarkjs/remark',
+  router5: 'https://github.com/router5/router5',
+  sentry: 'https://github.com/getsentry/sentry-javascript',
+  storybook: 'https://github.com/storybooks',
+  strapi: 'https://github.com/strapi/strapi',
+  stryker: 'https://github.com/stryker-mutator/stryker',
+  surveyjs: 'https://github.com/surveyjs/surveyjs',
+  typefaces: 'https://github.com/KyleAMathews/typefaces',
+  uppy: 'https://github.com/transloadit/uppy',
+  vue: 'https://github.com/vuejs/vue',
+  vuepress: 'https://github.com/vuejs/vuepress',
+  webdriverio: 'https://github.com/webdriverio/webdriverio',
+  workbox: 'https://github.com/googlechrome/workbox',
 };
 
-const dynamicSources = {
-  accounts: {
-    repo: 'accounts-js/accounts',
-  },
-  angular: {
-    repo: 'angular/angular',
-  },
-  awsappsync: {
-    repo: 'awslabs/aws-mobile-appsync-sdk-js',
-  },
-  apolloclient: {
-    repo: 'apollographql/apollo-client',
-    includePrivatePackage: true,
-  },
-  apollolink: {
-    repo: 'apollographql/apollo-link',
-    includePrivatePackage: true,
-  },
-  babel6: {
-    repo: 'babel/babel',
-    branch: '6.x',
-  },
-  babel: {
-    repo: 'babel/babel',
-  },
-  baset: {
-    repo: 'igmat/baset',
-  },
-  emotion: {
-    repo: 'emotion-js/emotion',
-  },
-  jest: {
-    repo: 'facebook/jest',
-  },
-  pouchdb: {
-    repo: 'pouchdb/pouchdb',
-    path: 'packages/node_modules',
-  },
-  react: {
-    repo: 'facebook/react',
-  },
-  reactrouter: {
-    repo: 'ReactTraining/react-router',
-  },
-  mui: {
-    repo: 'mui-org/material-ui',
-    includePrivatePackage: true,
-  },
-  gatsby: {
-    repo: 'gatsbyjs/gatsby',
-  },
-  material: {
-    repo: 'material-components/material-components-web',
-  },
-  'mdc-react': {
-    repo: 'material-components/material-components-web-react',
-  },
-  'graphql-modules': {
-    repo: 'Urigo/graphql-modules',
-  },
-  vue: {
-    repo: 'vuejs/vue',
-    includeRoot: true,
-  },
-  'vue-cli': {
-    repo: 'vuejs/vue-cli',
-    branch: 'dev',
-    path: 'packages/@vue',
-  },
+const patternGroups = {
+  wordpress: '^@wordpress/',
+  angularmaterial: ['^@angular/material', '^@angular/cdk'],
+  'aws-java-sdk': '^com.amazonaws:aws-java-sdk-',
 };
 
 async function go() {
   const config = {};
-  for (const monorepo of Object.keys(staticSources)) {
-    config[monorepo] = {
-      description: `${monorepo} monorepo`,
-      ...staticSources[monorepo],
+  for (const [name, value] of Object.entries(repoGroups)) {
+    config[name] = {
+      description: `${name} monorepo`,
+      sourceUrlPrefixes: is.array(value) ? value : [value],
     };
   }
-  for (const monorepo of Object.keys(dynamicSources)) {
-    const data = dynamicSources[monorepo];
-    const branch = data.branch || 'master';
-    const packagesPath = data.path || 'packages';
-    const includePrivatePackage = !!data.includePrivatePackage;
-    const url = `https://api.github.com/repos/${
-      data.repo
-    }/contents/${packagesPath}`;
-    const dirListing = (await got(url)).body;
-    const packages = [];
-    for (const item of dirListing) {
-      if (item.type === 'dir') {
-        const packageJsonUrl = `https://raw.githubusercontent.com/${
-          data.repo
-        }/${branch}/${packagesPath}/${item.name}/package.json`.replace(
-          /@/g,
-          '%40'
-        );
-        try {
-          const packageJson = (await got(packageJsonUrl)).body;
-          if (
-            packageJson.name &&
-            (includePrivatePackage || packageJson.private !== true)
-          ) {
-            packages.push(packageJson.name);
-          }
-        } catch (err) {
-          // No package.json
-        }
-      }
-    }
-    if (data.includeRoot) {
-      // Now check root of repo
-      try {
-        const packageJsonUrl = `https://raw.githubusercontent.com/${
-          data.repo
-        }/${branch}/package.json`;
-        const packageJson = (await got(packageJsonUrl)).body;
-        if (
-          packageJson.name &&
-          (includePrivatePackage || packageJson.private !== true)
-        ) {
-          packages.push(packageJson.name);
-        }
-      } catch (err) {
-        // No package.json
-      }
-    }
-    config[monorepo] = {
-      description: `${monorepo} monorepo`,
-      packageNames: packages,
+  for (const [name, value] of Object.entries(patternGroups)) {
+    config[name] = {
+      description: `${name} monorepo`,
+      packagePatterns: is.array(value) ? value : [value],
     };
   }
   // Write rules in alphabetical order
