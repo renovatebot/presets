81a951fcbd599ca46861c23b6d694342b022953b
Rhys Arkins <rhys@arkins.net>
2020-04-28 12:19:05 +0200

    default.json -> presets.json

---
+++ build/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs-extra');
+const is = require('@sindresorhus/is');
+const prettyJson = require('json-stringify-pretty-compact');
+
+const input = {
+  default: require('./default'),
+};
+
+function sort(inObj) {
+  const outObj = {};
+  for (const key of Object.keys(inObj).sort()) {
+    const val = inObj[key];
+    outObj[key] = is.object(val) && !is.array(val) ? sort(val) : val;
+  }
+  return outObj;
+}
+
+const output = sort(input);
+fs.writeFileSync('presets.json', prettyJson(output));
