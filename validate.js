394fd82f3e38e8c4c2135d4efbc578b94c8a5cfd
Rhys Arkins <rhys@arkins.net>
2020-04-29 09:04:26 +0200

    refactor and document

--- validate.js
+++ validate.js
@@ -1,43 +1,42 @@
 const fs = require('fs-extra');
+const is = require('@sindresorhus/is');
 const { validateConfig } = require('renovate/dist/config/validation');
 const { massageConfig } = require('renovate/dist/config/massage');
 
+// This is the file we're validating
 const presetsJson = JSON.parse(fs.readFileSync('presets.json', 'utf8'));
 
 global.repoCache = {
-  internalPresets: presetsJson,
+  internalPresets: presetsJson, // This makes renovate validate this file contents and not what's on GitHub currently
 };
 
 let returnVal = 0;
 
-async function validate(desc, config, isPreset = false) {
-  const res = await validateConfig(massageConfig(config), isPreset);
-  if (res.errors.length) {
-    console.log(
-      `${desc} contains errors:\n\n${JSON.stringify(res.errors, null, 2)}`
-    );
-    returnVal = 1;
-  }
-  if (res.warnings.length) {
-    console.log(
-      `${desc} contains warnings:\n\n${JSON.stringify(res.warnings, null, 2)}`
-    );
-    returnVal = 1;
-  }
-}
+// These ignoredRules container params (arguments) that fail to validate when unpopulated
+const ignoredRules = ['default:group', 'default:timezone'];
 
 (async () => {
   try {
     for (const [category, presets] of Object.entries(presetsJson)) {
       for (const [subcategory, preset] of Object.entries(presets)) {
         const name = `${category}:${subcategory}`;
-        const ignoredRules = ['default:group', 'default:timezone'];
         if (!ignoredRules.includes(name) && subcategory !== 'description') {
-          // console.log('Validating ' + name);
           try {
-            await validate(name, preset, true);
+            const { warnings, errors } = await validateConfig(
+              massageConfig(preset),
+              true
+            );
+            if (is.nonEmptyArray(warnings)) {
+              console.warn({ warnings }, 'Found renovate config warnings');
+              returnVal = 1;
+            }
+            if (is.nonEmptyArray(errors)) {
+              console.error({ errors }, 'Found renovate config errors');
+              returnVal = 1;
+            }
           } catch (err) {
-            console.log({ name, err });
+            console.error(`Failed to verify: ${name}`);
+            console.log(err);
             returnVal = 1;
           }
         }
